8e041c211a1624e2d606668a8043aaa0679eb2c3
Chris LaRose <cjlarose@gmail.com>
2016-07-06 18:58:56 -0700

    cjl - Optimize largest rectangle slightly

--- LargestRectangle.hs
+++ LargestRectangle.hs
@@ -3,14 +3,14 @@ module LargestRetangle where
 readInts :: String -> [Int]
 readInts = (map read) . words
 
-rectangles :: Int -> [Int] -> [[Int]]
+rectangles :: Int -> [Int] -> [Int]
 rectangles _ [] = []
-rectangles k heights = (map (* k) heights) : (rectangles (k + 1) pairwiseMinima) where
+rectangles k heights = (maximum heights * k) : (rectangles (k + 1) pairwiseMinima) where
   pairwiseMinima = zipWith min heights (tail heights)
 
 main = do
   _ <- getLine
   heightsLine <- getLine
   let heights = readInts heightsLine
-  let maxArea = (maximum . concat . (rectangles 1)) $ heights
+  let maxArea = (maximum . (rectangles 1)) $ heights
   putStr $ show maxArea
