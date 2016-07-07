35009b87f0474c274ec6d35bde4ccfd79130658c
Chris LaRose <cjlarose@gmail.com>
2016-07-06 20:19:27 -0700

    cjl - Implement LargestRectangle with a tail-call-recursive function

--- largest-rectangle/Main.hs
+++ largest-rectangle/Main.hs
@@ -1,16 +1,17 @@
 module Main where
 
 readInts :: String -> [Int]
-readInts = (map read) . words
+readInts = map read . words
 
-rectangles :: Int -> [Int] -> [Int]
-rectangles _ [] = []
-rectangles k heights = (maximum heights * k) : (rectangles (k + 1) pairwiseMinima) where
+rectangles :: Int -> Int -> [Int] -> Int
+rectangles _ maxArea [] = maxArea
+rectangles k maxArea heights = rectangles (k + 1) newMax pairwiseMinima where
   pairwiseMinima = zipWith min heights (tail heights)
+  newMax = max maxArea (maximum heights * k)
 
 main = do
   _ <- getLine
   heightsLine <- getLine
   let heights = readInts heightsLine
-  let maxArea = (maximum . (rectangles 1)) $ heights
+  let maxArea = rectangles 1 (head heights) heights
   putStr $ show maxArea
