dc4fb43225e2d2e319b6b099273f48524a8b9b98
Chris LaRose <cjlarose@gmail.com>
2016-07-10 10:45:52 -0700

    Remove some unnecessary reverses

--- waiter/Main.hs
+++ waiter/Main.hs
@@ -1,9 +1,15 @@
 module Main where
 
-import Data.List (partition)
+import Data.List (partition, foldl')
 
 readInts :: String -> [Int]
 readInts = map read . words
+
+partition' p = foldl' (select p) ([],[])
+  where
+    select p (t, f) x
+      | p x = (x : t, f)
+      | otherwise = (t, x : f)
 
 factors n = [d | d <- [2..n], n `mod` d == 0]
 
@@ -12,9 +18,9 @@ primes = [p | p <- [2..], null . tail . factors $ p]
 stackPlates q plates = fmt $ foldl f ([], reverse plates) (take q primes)
   where
     fmt (stacked, remaining) = (concat . reverse) stacked ++ remaining
-    f (stacked, plates) p = (reverse newStacked : stacked, reverse remaining)
+    f (stacked, plates) p = (newStacked : stacked, remaining)
       where
-        (newStacked, remaining) = partition (\x -> x `mod` p == 0) plates
+        (newStacked, remaining) = partition' (\x -> x `mod` p == 0) plates
 
 main = do
   inputSizeLine <- getLine
