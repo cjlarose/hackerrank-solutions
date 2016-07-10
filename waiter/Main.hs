f6d7589f50e9b4a8fee41b7507cacdc86f3234f9
Chris LaRose <cjlarose@gmail.com>
2016-07-09 20:30:30 -0700

    Complete waiter challenge

---
+++ waiter/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import qualified Data.Map as Map
+import Data.List (partition)
+
+readInts :: String -> [Int]
+readInts = map read . words
+
+primeFactors n = [d | d <- [2..n], n `mod` d == 0]
+
+primes = [p | p <- [2..], null . tail . primeFactors $ p]
+
+stackPlates q plates = fmt $ foldl f ([], reverse plates) (take q primes)
+  where
+    fmt (stacked, remaining) = (concat . reverse) stacked ++ remaining
+    f (stacked, plates) p = (reverse newStacked : stacked, reverse remaining)
+      where
+        (newStacked, remaining) = partition (\x -> x `mod` p == 0) plates
+
+main = do
+  inputSizeLine <- getLine
+  let (n:q:[]) = readInts inputSizeLine
+  platesLine <- getLine
+  let plates = readInts platesLine
+  mapM print $ stackPlates q plates
